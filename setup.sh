fb6646b1cc8de2e79d28c46b785a8deca7883763
Poorna K P <poorna.kp@natwest.com>
2024-10-08 18:19:31 +0530

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Auth0 Configuration
+export AUTH0_DOMAIN="myapp-secure.us.auth0.com"
+export AUTH0_CLIENT_ID="BxGTvu4My47OVTJPNi9ksg6OBNROf3qr"
+export AUTH0_CLIENT_SECRET="VHOnlzQW_M_SbJWmTVjr7_IoLtHbzLrn8Q780S367W_pcGi8ulSkVjVdTw5lQiPP"
+export AUTH0_CALLBACK_URL="https://127.0.0.1:8080/login-results"
+export AUTH0_LOGOUT_URL="https://127.0.0.1:8080/logout"
+export API_AUDIENCE="https://casting-agency-auth/"
